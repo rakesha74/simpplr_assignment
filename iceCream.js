45859cafc4324ae46a6c45a0bef303e3895bd80f
Rakesh Agarwal <rakesha74@gmail.com>
2020-10-11 18:44:29 +0530

    Adding logs

--- iceCream.js
+++ iceCream.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const client = require('./client');
+const util = require('util');
 
 const searchRequest = {
   location: 'Redwood City',
@@ -13,18 +14,22 @@ async function fetchTopIceCreamParlour(){
     try {
         var outputResult = [];
         var result = await client.search(searchRequest);
+
         if(result && result.jsonBody && result.jsonBody.businesses){
             var icecreamParlourResult = result.jsonBody.businesses;
         } else {
             throw(new Error("Response is empty"));
         }
-        
+
+        util.log("Top 10 Ice cream parlour result::"+JSON.stringify(icecreamParlourResult,undefined,2));
+
         for(var index=0;index<icecreamParlourResult.length;index++){
 
             var temp = {};
             temp.business_name = icecreamParlourResult[index]["name"];
             temp.business_address = (icecreamParlourResult[index]["location"] && 
                                     icecreamParlourResult[index]["location"]["display_address"]);
+            
             if(icecreamParlourResult[index].id){
                 
                 var reviewResult = await client.reviews(icecreamParlourResult[index].id);
@@ -34,7 +39,7 @@ async function fetchTopIceCreamParlour(){
 
                         var reviewResult = reviewResult.jsonBody["reviews"];
                         var reviews_array = [];
-
+                        util.log("Reviews for business id::"+icecreamParlourResult[index].id+"::"+JSON.stringify(reviewResult,undefined,2));
                         for(var reviewIndex=0; reviewIndex<reviewResult.length;reviewIndex++){
                             
                             var temp_review = {};
@@ -60,7 +65,7 @@ async function fetchTopIceCreamParlour(){
  
 
 fetchTopIceCreamParlour().then(result=>{
-    console.log(JSON.stringify(result,undefined,2)+"::"+result.length);
+    util.log("Final result fro Top 10 Ice cream Parlour with reviews::"+JSON.stringify(result,undefined,2));
 }).catch(e => {
-      console.log(e);
+      util.log("Error::"+JSON.stringify(e,undefined,2));
 });
