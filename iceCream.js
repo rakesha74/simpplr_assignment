cecc154ef09fdc5a953b5b343746290c9e5b0516
Rakesh Agarwal <rakesha74@gmail.com>
2020-10-11 14:52:56 +0530

    Fetching reviews for business

--- iceCream.js
+++ iceCream.js
@@ -7,23 +7,27 @@ const yelp = require('yelp-fusion');
 const apiKey = 'pSxxXBpunBnBT0mIoP_m6VTEy32nmGTOG8xO5_9ehd8uIPpSgZvaSTxqmVoNdR_OxuXI2u5JLB5gltsjBUxat5sy4ci96wvRmWJT_Bmw473B48rMEeZQml2lcPBeX3Yx';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
   location: 'Redwood City',
   categories: 'icecream',
   limit: 10,
   sort_by: "rating",
-
+  open_now: true
 };
 
 const client = yelp.client(apiKey);
 
 async function fetchTopIceCreamParlour(){
     var result = await client.search(searchRequest);
+    var icecreamParlourResult = result.jsonBody.businesses;
+    for(var index=0;index<icecreamParlourResult.length;index++){
+        var reviewsresult = await client.reviews(icecreamParlourResult[index].id);
+        console.log(JSON.stringify(reviewsresult.jsonBody,undefined,2));
+    }
     return result.jsonBody.businesses;
 }
  
 fetchTopIceCreamParlour().then(result=>{
-    console.log(JSON.stringify(result,undefined,2));
+    //console.log(JSON.stringify(result,undefined,2));
 }).catch(e => {
       console.log(e);
  });
