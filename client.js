739123fcd13d08f8940b82b3102378a1a13d45e2
Rakesh Agarwal <rakesha74@gmail.com>
2020-10-11 18:09:27 +0530

    Adding client

---
+++ client.js
@@ -0,0 +1,7 @@
+const yelp = require('yelp-fusion');
+
+const apiKey = 'pSxxXBpunBnBT0mIoP_m6VTEy32nmGTOG8xO5_9ehd8uIPpSgZvaSTxqmVoNdR_OxuXI2u5JLB5gltsjBUxat5sy4ci96wvRmWJT_Bmw473B48rMEeZQml2lcPBeX3Yx';
+
+const client = yelp.client(apiKey);
+
+module.exports = client;
